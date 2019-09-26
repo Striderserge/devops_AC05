2e10d1d7bfeb466a5cd9790a1c865d127a30b1dc
Striderserge <53881982+Striderserge@users.noreply.github.com>
2019-09-26 20:43:18 -0300

    Update test_convertehora.py
    corrigido falta de um teste

--- com/ope/test/test_convertehora.py
+++ com/ope/test/test_convertehora.py
@@ -9,5 +9,9 @@ def test_hora2():
     assert converte_hora.converteHora(10,10) == '10:10 AM', "deve ser 10:10AM"
 
 def test_hora3():
-    assert converte_hora.converteHora(23,10) == '11:10 PM', "deve ser 11:10PM"
+    assert converte_hora.converteHora(23,10) == '11:10 PM', "deve ser 11:10PM"  
+
+def test_hora4():
+    assert converte_hora.converteHora(0,10) == '12:10 AM', "deve ser 12:10AM"
+
 
