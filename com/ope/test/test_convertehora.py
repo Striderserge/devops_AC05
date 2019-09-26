7cbe90d4e011e16387fa80f11da59cd2c1140942
Striderserge <sergio.barros@aluno.faculdadeimpacta.com.br>
2019-09-26 20:25:55 -0300

    adicionado o programa converte hora

---
+++ com/ope/test/test_convertehora.py
@@ -0,0 +1,13 @@
+import pytest
+from com.ope import converte_hora
+
+
+def test_hora1():
+    assert converte_hora.converteHora(25,0) == None , "Deve retornar nulo"
+
+def test_hora2():
+    assert converte_hora.converteHora(10,10) == '10:10 AM', "deve ser 10:10AM"
+
+def test_hora3():
+    assert converte_hora.converteHora(23,10) == '11:10 PM', "deve ser 11:10PM"
+
